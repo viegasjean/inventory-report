d7a1be94b38414d2ffe9d56b36ff0c7d1b40abbc
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-20 15:17:47 -0300

    test: correct types of atributes

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -12,6 +12,20 @@ def test_cria_produto():
         "instrucao 1",
     )
 
+    assert type(product.id) == int
+
+    assert type(product.nome_do_produto) == str
+
+    assert type(product.nome_da_empresa) == str
+
+    assert type(product.data_de_fabricacao) == str
+
+    assert type(product.data_de_validade) == str
+
+    assert type(product.numero_de_serie) == str
+
+    assert type(product.instrucoes_de_armazenamento) == str
+
     assert (
         str(product)
         == "O produto Nicotine Polacrilex fabricado em 2021-02-18 por" +
