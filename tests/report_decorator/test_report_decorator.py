2d0114b6cb931807115995f0ac8acc3b0af7f042
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-21 19:46:56 -0300

    test: :test_tube: Test to colored_report decorator

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,41 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+stock = [
+  {
+    "id": "1",
+    "nome_do_produto": "Nicotine Polacrilex",
+    "nome_da_empresa": "Target Corporation",
+    "data_de_fabricacao": "2021-02-18",
+    "data_de_validade": "2023-09-17",
+    "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+    "instrucoes_de_armazenamento": "instrucao 1"
+  },
+  {
+    "id": "2",
+    "nome_do_produto": "fentanyl citrate",
+    "nome_da_empresa": "Target Corporation",
+    "data_de_fabricacao": "2020-12-06",
+    "data_de_validade": "2023-12-25",
+    "numero_de_serie": "FR29 5951 7573 74OY XKGX 6CSG D20",
+    "instrucoes_de_armazenamento": "instrucao 2"
+  },
+  {
+    "id": "3",
+    "nome_do_produto": "NITROUS OXIDE",
+    "nome_da_empresa": "Galena Biopharma",
+    "data_de_fabricacao": "2020-12-22",
+    "data_de_validade": "2024-11-07",
+    "numero_de_serie": "CZ09 8588 0858 8435 9140 2695",
+    "instrucoes_de_armazenamento": "instrucao 3"
+  },
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    simple_report = SimpleReport()
+    colored_report = ColoredReport(simple_report)
+    decorated_report = colored_report.generate(stock)
+    assert '\033[36m' in decorated_report
+    assert '\033[0m' in decorated_report
+    assert '\033[32m' in decorated_report
