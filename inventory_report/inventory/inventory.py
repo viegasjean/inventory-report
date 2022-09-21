c176a3af90a7b3e380f6b767af52f721e49a987d
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-21 18:52:38 -0300

    refactor: change order of methods

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,3 @@
-
-
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
@@ -7,18 +5,10 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
-class Inventory():
-    @classmethod
-    def import_data(cls, path: str, tipo: str):
-        produtos = cls.read_file(path)
-        if tipo == 'simples':
-            return SimpleReport.generate(produtos)
-        if tipo == 'completo':
-            return CompleteReport.generate(produtos)
-
+class Inventory:
     @classmethod
     def read_file(cls, path):
-        _, extensao = path.split('.')
+        _, extensao = path.split(".")
         if extensao == "csv":
             return CsvImporter.import_data(path)
         if extensao == "json":
@@ -26,3 +16,11 @@ class Inventory():
         if extensao == "xml":
             return XmlImporter.import_data(path)
         raise ValueError("Unknown extension")
+
+    @classmethod
+    def import_data(cls, path: str, tipo: str):
+        produtos = cls.read_file(path)
+        if tipo == "simples":
+            return SimpleReport.generate(produtos)
+        if tipo == "completo":
+            return CompleteReport.generate(produtos)
