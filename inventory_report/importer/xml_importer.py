f0571ea5f1afda1d58924b01131f0b8fdb5ce56d
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-21 17:36:19 -0300

    feat: :sparkles: make clases with Strategy

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,13 @@
+import xmltodict
+
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if not path.endswith('.xml'):
+            raise ValueError("Arquivo inválido")
+        with open(path) as file:
+            content = file.read()
+            return xmltodict.parse(content)['dataset']['record']
