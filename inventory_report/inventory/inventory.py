279577ef309317e1acde2dbef0d01b99237d741e
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-21 15:21:14 -0300

    feat(inventory): :sparkles: Import csv and generate report

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -7,9 +8,24 @@ from inventory_report.reports.simple_report import SimpleReport
 class Inventory():
     @classmethod
     def import_data(cls, path: str, tipo: str):
-        with open(path, encoding="utf-8") as file:
-            produtos = list(csv.DictReader(file))
-            if tipo == 'simples':
-                return SimpleReport.generate(produtos)
-            if tipo == 'completo':
-                return CompleteReport.generate(produtos)
+        if 'csv' in path:
+            with open(path, encoding="utf-8") as file:
+                produtos = list(csv.DictReader(file))
+                if tipo == 'simples':
+                    return SimpleReport.generate(produtos)
+                if tipo == 'completo':
+                    return CompleteReport.generate(produtos)
+        if 'json' in path:
+            with open(path) as file:
+                produtos = json.load(file)
+                if tipo == 'simples':
+                    return SimpleReport.generate(produtos)
+                if tipo == 'completo':
+                    return CompleteReport.generate(produtos)
+        if 'xml' in path:
+            with open(path) as file:
+                produtos = json.load(file)
+                if tipo == 'simples':
+                    return SimpleReport.generate(produtos)
+                if tipo == 'completo':
+                            return CompleteReport.generate(produtos)
