6eb5a7be9d502af939c380c560a43a181728eba8
Jean Viegas <jeanmelg@gmail.com>
2022-09-23 18:22:35 -0300

    feat: Inventory Iterator

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,14 @@
+from collections.abc import Iterator
+
+class InventoryIterator(Iterator):
+    def __init__(self, data):
+        self.data = data
+        self.curr = 0
+
+    def __next__(self):
+        try:
+            result = self.data[self.curr]
+        except IndexError:
+            raise StopIteration
+        self.curr += 1
+        return result
---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,17 @@
+from collections.abc import Iterable
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer: Importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path: str, tipo: str):
+        self.data += self.importer.import_data(path)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
