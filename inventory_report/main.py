ab897faebf9a6dd60f1712b2c671f8d627c9e560
Jean Viegas <jeanmelg@gmail.com>
2022-09-23 20:07:35 -0300

    feat:

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,19 @@
+import sys
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+
+    arquivo, caminho_do_arquivo_input, tipo_de_relatório = sys.argv
+
+    relatorio = Inventory.import_data(
+        caminho_do_arquivo_input, tipo_de_relatório
+    )
+    print(relatorio, end="")
+
+
+if __name__ == "__main__":
+    main()
